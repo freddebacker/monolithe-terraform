61ec6bcee10679a14d3ac9f3e1c255bad7b2bc80
Remi Vichery <remi.vichery@nuagenetworks.net>
2018-03-18 19:14:06 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='monolitheterraform',
+      version='0.1',
+      description='Terraform generator for monolithe',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests', '.git', '.gitignore', 'README.md']),
+      include_package_data=True,
+      
+      # This is very important!
+      #
+      # This is how Monolithe will find and decide to use your plugin.
+      # The entry point *MUST* be 'monolithe.plugin.lang.xxx' where 'xxx'
+      # is what the user will enter as value for the '--language' option.
+      # The the value *MUST* be an 'info:name_of_the_package:name_of_info_function'
+      # keep things simple, and always use these names. Just adapt the package name.
+      entry_points={'monolithe.plugin.lang.terraform': ['info=monolitheterraform:plugin_info']},
+      )
